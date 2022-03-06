0fbfa4ea997d02704c7398c2ecd5baf38862b8ae
Lohkdesgds <lohk.lohkdesgds@gmail.com>
2022-03-06 10:34:55 -0300

    Submodule update (as things get sliced)

--- include/Lunaris/Utility/bomb.h
+++ include/Lunaris/Utility/bomb.h
@@ -3,6 +3,4 @@
 // This will include all the respective folder for you
 
 #include "bomb/bomb.h"
-#ifdef LUNARIS_HEADER_ONLY
-#include "bomb/bomb.cpp"
-#endif
+#include "bomb/bomb.ipp"
--- include/Lunaris/Utility/mutex.h
+++ include/Lunaris/Utility/mutex.h
@@ -3,6 +3,4 @@
 // This will include all the respective folder for you
 
 #include "mutex/mutex.h"
-#ifdef LUNARIS_HEADER_ONLY
-#include "mutex/mutex.cpp"
-#endif
+#include "mutex/mutex.ipp"
